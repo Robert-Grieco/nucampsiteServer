c9e7d4633916ef65f220dcd0caa942b3beaa6a16
Robert Grieco <rgrieco@live.com>
2020-12-11 18:39:17 -0500

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: "12345-67890-09876-54321",
   mongoUrl: "mongodb://localhost:27017/nucampsite",
+  facebook: {
+    clientId: "386923049244580",
+    clientSecret: "3011c576684aecd1bfa9d5c1a2452042",
+  },
 };
